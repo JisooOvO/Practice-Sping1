38595b155ed60ee996e2b3ede37ef731a698b6a8
Amuz-Jisoo <jisoo@amuz.co.kr>
2025-03-02 17:39:59 +0900

    [Study] jdbc

--- src/main/java/com/example/hello_spring/SpringConfig.java
+++ src/main/java/com/example/hello_spring/SpringConfig.java
@@ -1,14 +1,25 @@
 package com.example.hello_spring;
 
+import com.example.hello_spring.repository.JdbcMemberRepository;
 import com.example.hello_spring.repository.MemberRepository;
 import com.example.hello_spring.repository.MemoryMemberRepository;
 import com.example.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 직접 등록
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -18,6 +29,11 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
         // 레포지토리를 변경해야 할 경우 이거만 변경
-        return new MemoryMemberRepository();
+
+        // Memory
+        // return new MemoryMemberRepository();
+
+        // JDBC
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- src/main/java/com/example/hello_spring/controller/MemberForm.java
+++ src/main/java/com/example/hello_spring/controller/MemberForm.java
@@ -1,13 +1,17 @@
 package com.example.hello_spring.controller;
 
+// modelAttribute로 동작
+// http 파라미터를 가져와서 필드와 매칭
 public class MemberForm {
-
+    // 프로퍼티 이름은 같을 필요 없으나
     private  String name;
 
     public String getName() {
         return name;
     }
 
+    // 자바 빈 규약의 세터와 파라미터 이름이 일치할 경우 자동으로 매칭
+    // 일치하는 세터가 없으면 null 반환
     public void setName(String name) {
         this.name = name;
     }
